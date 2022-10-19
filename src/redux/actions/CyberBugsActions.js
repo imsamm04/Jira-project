e67647a9101f953091086c2278f28e5c1ccbbc94
ducviet2504 <ducviet2504@gmail.com>
2022-10-19 21:11:51 +0700

    first commit

--- src/redux/actions/CyberBugsActions.js
+++ src/redux/actions/CyberBugsActions.js
@@ -1,13 +1,11 @@
-import { USER_SIGNIN_API } from "../constants/Cyberbugs/Cyberbugs"
-
-
+import { USER_SIGNIN_API } from "../constants/Cyberbugs/Cyberbugs";
 
 export const singinCyberbugAction = (email, password) => {
-    return {
-        type: USER_SIGNIN_API,
-        userLogin: {
-            email: email,
-            password: password
-        }
-    }
-}
+  return {
+    type: USER_SIGNIN_API,
+    userLogin: {
+      email: email,
+      password: password,
+    },
+  };
+};
--- src/templates/HomeTemplate/UserLoginTemplate.js
+++ src/templates/HomeTemplate/UserLoginTemplate.js
@@ -1,43 +1,50 @@
-import React,{useState,useEffect} from 'react';
-import { Route } from 'react-router-dom';
-import { Button,Layout } from 'antd';
+import React, { useState, useEffect } from "react";
+import { Route } from "react-router-dom";
+import { Button, Layout } from "antd";
 
 const { Header, Footer, Sider, Content } = Layout;
 
-
-
-
-
-
-
 export const UserLoginTemplate = (props) => {
+  const [{ width, height }, setSize] = useState({
+    width: Math.round(window.innerWidth),
+    height: Math.round(window.innerHeight),
+  });
 
-    const [{width,height},setSize] = useState({width:Math.round(window.innerWidth),height:Math.round(window.innerHeight)});
+  useEffect(() => {
+    window.onresize = () => {
+      setSize({
+        width: Math.round(window.innerWidth),
+        height: Math.round(window.innerHeight),
+      });
+    };
+  }, []);
 
-    useEffect(()=>{
-        window.onresize = () => {
-            setSize({
-                width: Math.round(window.innerWidth),
-                height:Math.round(window.innerHeight)
-            })
-        }
-    },[])
+  let { Component, ...restRoute } = props;
 
-
-    let {Component,...restRoute} =  props;
-
-    return <Route {...restRoute} render={(propsRoute)=>{
-        return <>
+  return (
+    <Route
+      {...restRoute}
+      render={(propsRoute) => {
+        return (
+          <>
             <Layout>
-                <Sider width={width/2} style={{height:height,backgroundImage:`url(https://picsum.photos/${Math.round(width/2)}/${height})`,backgroundSize:'100%'}}>
-                  
-                </Sider>
-                <Content>
-                    <Component {...propsRoute} />
-                </Content>
+              <Sider
+                width={width / 2}
+                style={{
+                  height: height,
+                  backgroundImage: `url(https://picsum.photos/${Math.round(
+                    width / 2
+                  )}/${height})`,
+                  backgroundSize: "100%",
+                }}
+              ></Sider>
+              <Content>
+                <Component {...propsRoute} />
+              </Content>
             </Layout>
-      
-        </>
-    }} />
-
-}
+          </>
+        );
+      }}
+    />
+  );
+};
