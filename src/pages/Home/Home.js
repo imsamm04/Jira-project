25389beea93c29b6f82811543f84bd00227c5fc7
ducviet2504 <ducviet2504@gmail.com>
2022-11-12 19:50:12 +0700

    update download file home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { CSVLink, CSVDownload } from "react-csv";
 
+
+const csvData = [
+  ["firstname", "lastname", "email"],
+  ["Ahmed", "Tomi", "ah@smthing.co.com"],
+  ["Raed", "Labes", "rl@smthing.co.com"],
+  ["Yezzi", "Min l3b", "ymin@cocococo.com"]
+];
 export default function Home(props) {
   const userLogin = useSelector(
     (state) => state.UserLoginCyberBugsReducer.userLogin
   );
   return (
     <div>
+      <CSVLink data={csvData}>Download me</CSVLink>;
+
       {userLogin?.name}
       <img src={userLogin?.avatar} alt="true" />
     </div>
